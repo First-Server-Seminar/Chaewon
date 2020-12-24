ed5fc93df5ae5746f7e2389fc3e7caa6140874d2
chaeppy <chaeppy108@gmail.com>
2020-12-24 14:52:38 +0900

    2st-homework level3

--- homework/2st-homework/level3/asyncFsCrypto.js
+++ homework/2st-homework/level3/asyncFsCrypto.js
@@ -6,11 +6,11 @@ crypto.randomBytes(64, (err, buf) => {
     const salt = buf.toString('base64');
     console.log(`salt : ${salt}`);
 
-    crypto.pbkdf2('비밀번호', salt, 100000, 64, 'sha512', (err, key) => {
+    crypto.pbkdf2('123456', salt, 100000, 64, 'sha512', (err, key) => {
         const password = `비밀번호 : ${key.toString('base64')}`;
 
         fs.writeFile('비밀번호.txt', password, () => {
-            console.log(`비밀번호 : ${key.toString('base64')}`);
+            console.log(password);
         });
     });
 });
